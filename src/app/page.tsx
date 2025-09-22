baca3bb3d6ae6b680a1a85ea7021f87caad39eed
Alejandro Saldaña <saldanaalejandro1410@gmail.com>
2025-09-22 08:51:18 +0000

    si que todo lo que borraste aparesca despues de seleccionar emprendeIA m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { AppHeader } from "@/components/app/header";
+import { MainContent } from "@/components/app/main-content";
+import { PricingSection } from "@/components/app/pricing-section";
+import { AppFooter } from "@/components/app/footer";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <main className="flex flex-1 items-center justify-center">
+      <main className="flex flex-1 flex-col items-center justify-center">
         <div className="container mx-auto max-w-6xl px-4 py-8">
-          <AppHeader />
+          <Accordion type="single" collapsible defaultValue="item-1" className="w-full">
+            <AccordionItem value="item-1" className="border-b-0">
+              <AccordionTrigger className="hover:no-underline">
+                <div className="flex-grow">
+                  <AppHeader />
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="space-y-12 pt-8">
+                  <MainContent />
+                  <PricingSection />
+                  <AppFooter />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </main>
     </div>
