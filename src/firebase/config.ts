25d76955b90ae4eb2b406dfeb5f8885e88888a3d
Alejandro Saldaña <saldanaalejandro1410@gmail.com>
2025-10-11 07:54:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3959207469-af070",
+  "appId": "1:397859853053:web:73ad74ce7739c91d6ae494",
+  "apiKey": "AIzaSyAgSPMXUVdKfGLb91DZNnwlt2tvfKGD7M8",
+  "authDomain": "studio-3959207469-af070.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "397859853053"
+};
