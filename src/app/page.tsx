1c57e55a4857b0523e1fc2527bc8c112bf3a2bbd
Alejandro Saldaña <saldanaalejandro1410@gmail.com>
2025-10-11 07:56:23 +0000

    Try fixing this error: `Console Error: In HTML, <button> cannot be a des

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,19 @@ import { AppHeader } from "@/components/app/header";
 import { MainContent } from "@/components/app/main-content";
 import { PricingSection } from "@/components/app/pricing-section";
 import { AppFooter } from "@/components/app/footer";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <main className="flex flex-1 flex-col items-center justify-center">
+      <main className="flex flex-1 flex-col items-center">
         <div className="container mx-auto max-w-6xl px-4 py-8">
+          <AppHeader />
+          <Separator className="my-8" />
           <Accordion type="single" collapsible defaultValue="item-1" className="w-full">
             <AccordionItem value="item-1" className="border-b-0">
-              <AccordionTrigger className="hover:no-underline">
-                <div className="flex-grow">
-                  <AppHeader />
-                </div>
-              </AccordionTrigger>
               <AccordionContent>
-                <div className="space-y-12 pt-8">
+                <div className="space-y-12">
                   <MainContent />
                   <PricingSection />
                   <AppFooter />
