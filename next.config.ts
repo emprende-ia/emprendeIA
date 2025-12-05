4eba06452bc63c1735dd8a838ce520ef2577cce8
Alejandro Saldaña <saldanaalejandro1410@gmail.com>
2025-12-05 02:58:15 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https://',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -36,9 +36,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       }
     ],
-  },
-  devIndicators: {
-    hotModuleReplacement: false,
   },
 };
 
