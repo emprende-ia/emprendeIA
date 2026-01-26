b2b2102b7133cce336f87e69438416af5a2e1fba
Alejandro Saldaña <saldanaalejandro1410@gmail.com>
2026-01-26 20:55:03 +0000

    sigue igual

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@
 const nextConfig = {
   swcMinify: false,
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/privacidad',
+        destination: '/privacidad.html',
+      },
+      {
+        source: '/terminos',
+        destination: '/terminos.html',
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
