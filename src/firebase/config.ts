f7ebd7b101e7957dcb7129bcfe1c1f855f5a6e08
Alejandro Saldaña <saldanaalejandro1410@gmail.com>
2026-01-27 01:24:43 +0000

    apiKey: "AIzaSyCWyEU7Fn4JxFYXH0fkm7VX-6MjhTzoRdo",   authDomain: "empren

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-3959207469-af070",
-  "appId": "1:397859853053:web:f1e1b12916e2f0dc6ae494",
-  "apiKey": "AIzaSyAgSPMXUVdKfGLb91DZNnwlt2tvfKGD7M8",
-  "authDomain": "studio-3959207469-af070.firebaseapp.com",
-  "storageBucket": "studio-3959207469-af070.appspot.com",
-  "measurementId": "G-XLKDG5KZ69",
-  "messagingSenderId": "397859853053"
+  "apiKey": "AIzaSyCWyEU7Fn4JxFYXH0fkm7VX-6MjhTzoRdo",
+  "authDomain": "emprendeia-clone.firebaseapp.com",
+  "projectId": "emprendeia-clone",
+  "storageBucket": "emprendeia-clone.appspot.com",
+  "messagingSenderId": "104770547655",
+  "appId": "1:104770547655:web:d9545564e4f0a8f839b3e2",
+  "measurementId": "G-KECCKZ4FDL"
 };
